8533216d19fab756e1a60b72d52bc0ed25117212
Kashif Minhaj <dailypctips@gmail.com>
2018-01-20 15:59:40 +0530

    Add DataService and list project data in project component

---
+++ src/app/data.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class DataService {
+
+  constructor() { }
+
+  getProjectItems() {
+    let projectItems: Array<ProjectItem> = [{
+      title: "Placements App",
+      description: "An android app for dept of Placements at NMAMIT",
+      link: "http://github.com/kashifmin",
+      image: "https://semantic-ui.com/images/avatar2/large/kristy.png",
+      date: "July 2016"
+    }];
+    return projectItems;
+  }
+
+}
--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,15 +1,21 @@
+import { DataService } from './../data.service';
 import { Component, OnInit } from '@angular/core';
+import { ProjectItem } from '../models/project-item';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.css']
+  styleUrls: ['./projects.component.css'],
+  providers: [ DataService ]
 })
 export class ProjectsComponent implements OnInit {
 
-  constructor() { }
+  projectItems: Array<ProjectItem>;
+
+  constructor(private data: DataService) { }
 
   ngOnInit() {
+    this.projectItems = this.data.getProjectItems();
   }
 
 }
