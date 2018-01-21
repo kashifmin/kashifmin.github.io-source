03341dad81f6b8998232515e88864dfe90fd63f4
Kashif Minhaj <dailypctips@gmail.com>
2018-01-21 11:02:54 +0530

    Add more data and fix styles issues

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -7,13 +7,24 @@ export class DataService {
   constructor() { }
 
   getProjectItems() {
-    let projectItems: Array<ProjectItem> = [{
-      title: "Placements App",
-      description: "An android app for dept of Placements at NMAMIT",
-      link: "http://github.com/kashifmin",
-      image: null,
-      date: "July 2016"
-    }];
+    let projectItems: Array<ProjectItem> = [
+      {
+        title: "Placements App",
+        description: "An android app for dept of Placements at NMAMIT",
+        link: "http://github.com/kashifmin",
+        image: null,
+        date: "July 2016"
+      },
+      {
+        title: "AIDA - Artificially Intelligent Digital Assistant",
+        description: `A smart assistant and a platform that servers machine learning APIs for 
+        object recognition, automated text responses, voice control, action recognition, face recognition,
+        basic smart assistant actions etc`,
+        link: "http://github.com/kashifmin",
+        image: null,
+        date: "September 2017 - Ongoing"
+      }
+  ];
     return projectItems;
   }
 
